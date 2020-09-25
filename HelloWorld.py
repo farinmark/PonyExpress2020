b025002587e7c89d9825f0007f9835ee09770d60
farinmark <65446586+farinmark@users.noreply.github.com>
2020-09-25 17:54:17 +0300

    Add files via upload

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,8 @@
-def Chet(a):
-    if a%2==1:
-        return "no"
-    else :
-        return "yes"
+def Square(a):
+    b=list()
+    b.append(a*4)
+    b.append(a*a)
+    b.append(a*(2**(1/2)))
+    return b
 a=int(input())
-print(Chet(a))
+print(Square(a))
