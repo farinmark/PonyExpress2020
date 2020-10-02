ecbba8d5e50f39ffa8dd4db7760c48872e176b4f
farinmark <65446586+farinmark@users.noreply.github.com>
2020-10-02 18:29:03 +0300

    Update SecondCase.py

--- SecondCase.py
+++ SecondCase.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 def SecondCase():
     driver_path = r'C:\Users\User\Desktop\python\chromedriver.exe'
@@ -16,12 +17,19 @@ def SecondCase():
     element_password.send_keys("rg#P5hZm4F")
     element_buttom.click()
 
+    time.sleep(5)
+
     try:
         element_menuButtom = driver.find_element_by_class_name("bp3-button-text")
     except:
+        driver.close()
         return "Wrong login or password"
+    time.sleep(10)
     try:
-        element_QuitFromPage = driver.find_element_by_xpath("M7 14H2V2h5c.55 0 1-.45 1-1s-.45-1-1-1H1C.45 0 0 .45 0 1v14c0 .55.45 1 1 1h6c.55 0 1-.45 1-1s-.45-1-1-1zm8.71-6.71l-3-3a1.003 1.003 0 00-1.42 1.42L12.59 7H6c-.55 0-1 .45-1 1s.45 1 1 1h6.59l-1.29 1.29c-.19.18-.3.43-.3.71a1.003 1.003 0 001.71.71l3-3c.18-.18.29-.43.29-.71 0-.28-.11-.53-.29-.71z")
+        element_QuitFromPage = driver.find_element_by_xpath("/html/body/div[1]/section/section[1]/div/div/div[2]/div/button[2]/span/svg")
+        element_QuitFromPage.click()
     except:
+        driver.close()
         return "Can't click on Quit buttom"
+    driver.close()
     return"Success"
