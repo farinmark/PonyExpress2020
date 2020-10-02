514a1c2024130be630168844d70c91df5d3ffd89
farinmark <65446586+farinmark@users.noreply.github.com>
2020-10-02 17:54:30 +0300

    Add files via upload

--- 02_10_2020.py
+++ 02_10_2020.py
@@ -1,23 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver_path = r'C:\Users\User\Desktop\python\chromedriver.exe'
-driver = webdriver.Chrome(executable_path=driver_path)
-driver.get('http://pegasus-edu.pegasus.ponyex.local/')
+def FirstCase():
+    driver_path = r'C:\Users\User\Desktop\python\chromedriver.exe'
+    driver = webdriver.Chrome(executable_path=driver_path)
+    driver.get('http://pegasus-edu.pegasus.ponyex.local/')
 
-element_login = driver.find_element_by_name("login")
-element_password = driver.find_element_by_name("password")
-element_buttom = driver.find_element_by_class_name("css-1hnkt5t")
+    element_login = driver.find_element_by_name("login")
+    element_password = driver.find_element_by_name("password")
+    element_buttom = driver.find_element_by_class_name("css-1hnkt5t")
 
-element_login.send_keys("ext.mgu_education")
-element_password.send_keys("rg#P5hZm4F")
-element_buttom.click()
+    element_login.send_keys("ext.mgu_education")
+    element_password.send_keys("rg#P5hZm4F")
+    element_buttom.click()
 
-try:
-    element_menuButtom = driver.find_element_by_class_name("bp3-button-text")
-except:
-    print("Wrong login or password")
-try:
-    element_Wrong = driver.find_element_by_class_name("css-hnp1e7")
-except:
-    print("Success")
+    try:
+        element_menuButtom = driver.find_element_by_class_name("bp3-button-text")
+    except:
+        return "Wrong login or password"
+    try:
+        element_Wrong = driver.find_element_by_class_name("css-hnp1e7")
+    except:
+        return "Success"
