183ab5d7a245c989a35f7b5cd3dd9031d5e962b3
farinmark <65446586+farinmark@users.noreply.github.com>
2020-10-16 18:13:28 +0300

    Add files via upload

--- 09_10_20.py
+++ 09_10_20.py
@@ -176,12 +176,110 @@ def fifth_case(driver_path, Login, Password):
     driver.close()
     return "Success"
 
+def menu_button_click(driver_path, Login, Password):
+    driver = authorization_in_pony(driver_path, Login, Password)
+    if driver == "FAIL ON STAGE: Wrong login or password" or driver == "No such input fields" or driver == "FAIL ON STAGE: Open page":
+        return driver
+    else:
+        print("Authorization success")
 
+    element_menu_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/section/section[1]/div/div/span/span/button')))
+    element_menu_button.click()
+    try:
+        element_servise_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div/div/div/div[2]/ul/li[7]/span/span/a/div')))
+    except:
+        print("No tab after menu button click")
+        driver.close()
+        return "FAIL ON STAGE: Click menu button"
 
+    print("Menu button click")
+    return driver
+
+def sixth_case(driver_path, Login, Password):
+    driver = menu_button_click(driver_path, Login, Password)
+    if driver == "FAIL ON STAGE: Wrong login or password" or driver == "No such input fields" or driver == "FAIL ON STAGE: Open page" or driver == "FAIL ON STAGE: Click menu button":
+        return driver
+
+    element_servise_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div/div/div[2]/ul/li[7]/span/span/a/div')))
+
+    element_servise_button.click()
+
+    try:
+        element_Permissions_management_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH,
+                                            '/html/body/div[3]/div/div/div/div[2]/ul/li[7]/span/div/div/div/div/ul/li[1]/span/span/a/div')))
+    except:
+        print("No tab after servise button click")
+        driver.close()
+        return "FAIL ON STAGE: Click servise button"
+
+    print("Servis button click")
+    element_Permissions_management_button.click()
+
+    try:
+        element_Group_of_users_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH,
+                                            '/html/body/div[3]/div/div/div/div[2]/ul/li[7]/span/div/div/div/div/ul/li[1]/span/div/div/div/div/ul/li[1]/a/div')))
+    except:
+        print("No tab after Permissions management click")
+        driver.close()
+        return "FAIL ON STAGE: Click Permissions management button"
+
+    print("Permissions management button click")
+    element_Group_of_users_button.click()
+
+    try:
+        element_create_group_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH,
+                                            '/html/body/div[1]/section/section[2]/section/section/div/div/div[1]/div[1]/button[1]/span[2]')))
+    except:
+        print("No page after group of users button click")
+        driver.close()
+        return "FAIL ON STAGE: Click group of users button button"
+
+    print("Group of users click")
+
+    element_create_group_button.click()
+    try:
+        element_create_group_name_field = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH,
+                                            '/html/body/div[3]/div/div[2]/div/div[1]/h4/span')))
+    except:
+        print("No create group window")
+        driver.close()
+        return "FAIL ON STAGE: Click on create group button "
+
+    element_create_group_name_field.send_keys(Password)
+    element_save_group_name_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH,'/html/body/div[3]/div/div[2]/div/div[2]/form/div[2]/button[1]/span')))
+    element_save_group_name_button.click()
+    try:
+        element_search_field = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH,'/html/body/div[1]/section/section[2]/section/section/div/div/div[1]/div[2]/div/div/div/input')))
+    except:
+        print("Save group button wasn't cliked")
+        driver.close()
+        return "FAIL ON STAGE: Click on save group button"
+    element_search_field.send_keys(Password)
+    element_page_counter =  WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH,'/html/body/div[1]/section/section[2]/section/section/div/div/div[2]/div[2]/div[1]/span/strong')))
+    Page_counter = element_page_counter.text
+    if Page_counter == "1 из 1":
+        print("Group create success")
+    else:
+        print("Group wasn't created")
+        driver.close()
+        return "FAIL ON STAGE: Create group"
+    print("6 case success")
+    driver.close()
+    return "success"
 
 
 if __name__ == "__main__":
     Login = "ext.mgu_education"
     Password = "rg#P5hZm4F"
     driver_path = r'C:\Users\User\Desktop\python\chromedriver.exe'
-    fifth_case(driver_path, Login, Password)
+    print(sixth_case(driver_path, Login, Password))
